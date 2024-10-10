ba8e4feddf5987abd8dfa75dcc56d30002f19d2b
AyushPatelSectionD <n01686702@humber.ca>
2024-10-10 18:46:20 -0400

    Create lab6.js

---
+++ lab6.js
@@ -0,0 +1,19 @@
+const myNumber = 10 
+const myString = ("Hello, JavaScript!")
+console.log(myNumber)// prints 10
+console.log(myString)// prints "Hello, JavaScript!"" 
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i) //prints 0-10
+}
+for (let i = 1; i <= 5; i++) {
+    console.log(3*i) //prints 3, 6, 9, 12, 15 
+}
+function addNumbers (num1, num2) {
+    return num1 + num2
+}
+console.log(addNumbers(8,7))//prints 15
+function multiplyNumbers (num1,num2) {
+return num1 * num2
+}
+console.log(multiplyNumbers(7,4)) //prints 28
